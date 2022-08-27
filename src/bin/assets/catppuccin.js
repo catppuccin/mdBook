caedfd094ae0be4421cf4f235ab80dfdfb929c33
sgoudham <sgoudham@gmail.com>
2022-08-27 09:41:34 +0100

    fix: Update assets and resolve highlights

--- src/bin/assets/catppuccin.js
+++
@@ -1,9 +0,0 @@
-var flavours = ["Latte", "Frappe", "Macchiato", "Mocha"];
-var theme_list = document.getElementById("theme-list");
-
-for (let flavour of flavours) {
-    var theme = document.createElement("li");
-    theme.setAttribute("role", "none");
-    theme.innerHTML = `<button role="menuitem" class="theme" id="${flavour.toLowerCase()}">${flavour}</button>`;
-    theme_list.appendChild(theme);
-}
